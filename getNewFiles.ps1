eb833ee7d732145a3f681ea1e630b723dbbfd375
Harvey West <harvey_west@live.com>
2017-07-31 18:00:20 +0100

    Improved logging

--- getNewFiles.ps1
+++ getNewFiles.ps1
@@ -11,7 +11,7 @@ PARAM(
 Import-Module -Name "./logger.psm1" #import standard logging module
 $currentFilePath = (Get-Item -Path ".\" -Verbose).FullName
 $logFilePath = $currentFilePath + "\Logs\" #All log files to be written to this location
-$debug = $true #If true, only output to console not to log file
+$debug = $false #If true, only output to console not to log file
 <#
 .Synopsis
     Writes a string to host if in debug, else use module logger to write to log file.
@@ -24,7 +24,7 @@ function log($outputString){
     }
 }
 $dateTargetFilePath = $targetFilePath + "\" + (Get-Date -format u).substring(0,10) + "\"
-log($dateTargetFilePath)
+log("Begin copying to: "+$dateTargetFilePath)
 mkdir -Force $dateTargetFilePath | Out-Null
 $existingFiles = Get-ChildItem $targetFilePath -Recurse -Filter "*.png" #Find all existing files with .png extension
 log(""+($existingFiles.Count)+" files exist already")
@@ -38,5 +38,10 @@ $fileSizeBytesMinimum = 200*1000 #File size minimum to try and eliminate non 16-
 $filteredFiles = $newFiles | Where-Object {$_.Name -notin ($existingFiles.Name.Replace(".png", "")) -and $_.Length -gt $fileSizeBytesMinimum}
 log(""+($filteredFiles.Count)+ " unique files greater than " + $fileSizeBytesMinimum + "B")
 
-#For each filtered file, copy the item to a folder and append ".png" to each file
-$filteredFiles | ForEach-Object{ $_.FullName | Copy-Item -Destination (Join-Path $dateTargetFilePath ($_.Name+".png"))}
+try{
+    #For each filtered file, copy the item to a folder and append ".png" to each file
+    $filteredFiles | ForEach-Object{ log("Beginning to copy file:" + $_.Name); $_.FullName | Copy-Item -Destination (Join-Path $dateTargetFilePath ($_.Name+".png"))}
+} catch {
+    log("Error copying file to: " + $dateTargetFilePath)
+}
+
